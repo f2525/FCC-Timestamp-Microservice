11c3f8ae00417436187d419b2eed5aa1966fadab
f2525 <62139970+f2525@users.noreply.github.com>
2021-03-26 23:48:07 +0700

    add testkit for FCC evaluation

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/index.html");
 })
